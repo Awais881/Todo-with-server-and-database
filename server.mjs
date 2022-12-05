957ebaf9d8e990c8ea680367370e51192274b6ef
Muhammad Awais <alizayn881@gmail.com>
2022-12-05 19:18:07 +0500

    code

--- server.mjs
+++ server.mjs
@@ -1,1 +1,48 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(cors());
+
+
+let todoSchema = new mongoose.Schema({
+    text: { type: String, required: true },
+    classId: String,
+    createdOn: { type: Date, default: Date.now }
+});
+
+
+let dbURI = 'mongodb+srv://awais881:awais123@cluster0.gq9n2zr.mongodb.net/abcdatabase?retryWrites=true&w=majority';
+mongoose.connect(dbURI);
+
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function() {//connected
+    console.log("Mongoose is connected");
+    // process.exit(1);
+});
+
+mongoose.connection.on('disconnected', function() {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function(err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function() {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function() {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
+let baseUrl = ``;
+if (window.location.href.split(":")[0] === "http") {
+  baseUrl = `http://localhost:3000`;
+}
 function App() {
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+     
+   
+
+
+   
+   
+   
+   
+   
+   
+   </>
   );
 }
 
